6ce33eb1c1f63bce68b06857a3b3de0fdb64a3e0
Michelle Elias Flores <michelleeliasflores@gmail.com>
2022-08-02 13:39:01 -0400

    Initial Commit

---
+++ githubpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubpractice
+//
+//  Created by Michelle Elias Flores on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
